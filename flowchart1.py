d53ff72d5f16e2febb5f7bd06533a25701c439b7
arabaliasqar6-cmyk <arabaliasqar6@gmail.com>
2025-12-22 14:50:09 +0330

    Add files via upload

---
+++ flowchart1.py
@@ -0,0 +1,4 @@
+a=int(input("enter the number1:"))
+b=int(input("enter the number2:"))
+average= (a+b)/2
+print("the average is",average)
---
+++ flowchart2.py
@@ -0,0 +1,5 @@
+M=int(input("enter the number:"))
+if M%2==0:
+        print("number is even")
+else:
+         print("number is odd")
