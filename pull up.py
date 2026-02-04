954f3fc815c16550930545989fbed377b3a19c52
arabaliasqar6-cmyk <arabaliasqar6@gmail.com>
2026-02-04 17:40:53 +0330

    Add files via upload

---
+++ pull up.py
@@ -0,0 +1,10 @@
+#40426993
+#علی اصغر عرب 
+#بارفیکس 
+n= int(input("Day: "))
+while n> 7 : 
+	n= int(input ("Choese a Day : "))
+else:
+	file = open("txt.pull up")
+	a = file.readlines()
+	print(a[n])
